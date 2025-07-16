7f692557fb781fd13ecb081bc0522749e54ced48
tanbiralam <itzmetanbir@gmail.com>
2025-07-17 00:19:05 +0530

    Add project management features and integrate message handling
    - Introduced Project model with relationships to Message model in Prisma schema.
    - Updated API to create and manage projects, including project ID handling in messages.
    - Enhanced front-end to support project creation and display project ID.
    - Added random-word-slugs dependency for project name generation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,41 @@
 "use client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
 
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("Message Created");
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
       },
     })
   );
 
   return (
-    <div className="p-4 max-w-7-xl mx-auto">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button
-        disabled={createMessage.isPending}
-        onClick={() => createMessage.mutate({ value: value })}
-      >
-        Invoke Background Jobs
-      </Button>
-      {JSON.stringify(messages, null, 2)}
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={createProject.isPending}
+          onClick={() => createProject.mutate({ value: value })}
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
+import { projectsRouter } from "@/modules/projects/server/procedures";
 import { createTRPCRouter } from "../init";
 import { messagesRouter } from "@/modules/messages/server/procedures";
 
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter,
 });
 export type AppRouter = typeof appRouter;
