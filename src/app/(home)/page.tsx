a117b56deaba04b9059017f8af55f2ca32903947
tanbiralam <itzmetanbir@gmail.com>
2025-08-26 00:15:28 +0530

    homepage completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectForm } from "@/modules/home/ui/components/project-form";
+import { ProjectsList } from "@/modules/home/ui/components/projects-list";
 import Image from "next/image";
 
 const Page = () => {
@@ -24,6 +25,8 @@ const Page = () => {
           <ProjectForm />
         </div>
       </section>
+
+      <ProjectsList />
     </div>
   );
 };
